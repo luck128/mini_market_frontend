b24764ef6c7aec286c75229c5c6fb054f8dc448f
Lucas da Silva Oliveira <lucas.oliveira@SHOPPER.CORP>
2024-11-16 08:48:40 -0300

    fix: corrigido o export das funções de maneira incorreta e retorno dos dados

--- src/services/Cart/cart.services.js
+++ src/services/Cart/cart.services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-exports.getProductsListService = async () => {
+export const getProductsListService = async () => {
     try {
         const response = await axios({
             method: 'GET',
@@ -17,34 +17,14 @@ exports.getProductsListService = async () => {
     }
 }
 
-exports.addToCartService = async (productId) => {
+export const addToCartService = async (productId) => {
     try {
         const response = await axios({
             method: 'GET',
             url: `http://localhost:3030/api/v1/products/${productId}`
         });
-        const newProduct = response.data;
-
-        if(newProduct[0].quantity !== 0) {
-            setCart((prevCart) => {
-                const existingProduct = prevCart.find(item => item.id === newProduct[0].id);
-    
-                if (existingProduct) {
-                    return prevCart.map(item =>
-                        item.id === newProduct[0].id
-                            ? { ...item, quantity: item.quantity + 1 }
-                            : item
-                    );
-                } else {
-                    return [...prevCart, { ...newProduct[0], quantity: 1, qty_stock: newProduct[0].quantity }];
-                }
-            });
-    
-            localStorage.setItem('cart', JSON.stringify(cart));
-            return 'Produto adicionado ao carrinho!';
-        } else {
-            return 'O estoque do produto acabou, tente procurar outro semelhante.';
-        }
+        
+        return response.data;
     } catch (error) {
         return error;
     }
